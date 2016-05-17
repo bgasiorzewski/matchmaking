ee1a287575e889a1ceac283d8b2ff879ba30aa93
Bartek Gąsiorzewski <bartek@yahoo.com>
2016-05-17 22:26:59 +0200

    Update top 500 once more

---
+++ top.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+~/.local/bin/matchmaking input/na0.html > input/na
+~/.local/bin/matchmaking input/na1.html >> input/na
+~/.local/bin/matchmaking input/na2.html >> input/na
+~/.local/bin/matchmaking input/na3.html >> input/na
+~/.local/bin/matchmaking input/na4.html >> input/na
+
+echo 'NA players (should be 500):'
+< input/na sort | uniq | wc
+
+~/.local/bin/matchmaking input/eu0.html > input/eu
+~/.local/bin/matchmaking input/eu1.html >> input/eu
+~/.local/bin/matchmaking input/eu2.html >> input/eu
+~/.local/bin/matchmaking input/eu3.html >> input/eu
+~/.local/bin/matchmaking input/eu4.html >> input/eu
+
+echo 'EU players (should be 500):'
+< input/eu sort | uniq | wc
