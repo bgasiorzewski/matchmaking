9b899e3154c3cb1cb21505883bd9fdb15635be24
Bartek Gąsiorzewski <bartek@yahoo.com>
2016-06-07 20:06:14 +0200

    Stop compensating for scraper crash

--- Matchmaking/Scraper.hs
+++ Matchmaking/Scraper.hs
@@ -86,7 +86,6 @@ handleTask manager (FetchMatch reg played hMatch) = do
 handleTask manager (FetchLastMatch gp) = do
     history <- fetchHistory manager gp
     handleMatch $ extractMatchId history 0
-    handleMatch $ extractMatchId history 5
     savePersist $ cyclSucc gp
     where
     handleMatch (hMatch, played) = do
